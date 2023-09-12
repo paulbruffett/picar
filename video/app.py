7b1c67e3e8579db03cc484dcaf5ab22f87539dac
Paul Bruffett <paulbruffett@protonmail.com>
2023-09-12 07:18:30 -0700

    longer wait time

--- video/app.py
+++ video/app.py
@@ -27,7 +27,7 @@ def gen_frames():  # generate frame by frame from camera
         else:
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
-            time.sleep(0.05)
+            time.sleep(0.1)
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
