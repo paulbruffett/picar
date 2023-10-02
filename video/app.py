e197033f156aa558c980d0341f604b54ce5805de
Paul Bruffett <paulbruffett@protonmail.com>
2023-10-02 16:54:48 -0700

    updated video streamings

--- video/app.py
+++ video/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, Response, jsonify, request
-import cv2
 import time
 import picar
 from picar import back_wheels, front_wheels
+import stream
 
 picar.setup()
 
@@ -13,29 +13,10 @@ fw.ready()
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)
+print(stream.start())
 
 SPEED = 60
 bw_status = 0
-
-def gen_frames():  # generate frame by frame from camera
-    while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            time.sleep(0.05)
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
-
-@app.route('/video_feed')
-def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/move', methods=['POST']) 
 def move():
@@ -71,8 +52,9 @@ def move():
 
 @app.route('/')
 def index():
+    host = stream.get_host().decode('utf-8').split(' ')[0]
     """Video streaming home page."""
-    return render_template('index.html')
+    return render_template('index.html', {'host': host})
 
 
 if __name__ == '__main__':
